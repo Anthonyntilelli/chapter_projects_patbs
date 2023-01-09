f09662144868fc626bd968bee71a6ec917cbfe75
Anthony Tilelli <Anthony@tilelli.me>
2023-01-08 20:31:55 -0500

    link_verification done

--- chapter_12/link_verification.py
+++ chapter_12/link_verification.py
@@ -1,16 +1,43 @@
 #!/usr/bin/env python
 
-import sys, requests
+import sys, requests, bs4
 
 if len(sys.argv) != 2:
-    print("Please add 1 url in as a command line argument (do not include https://)")
+    print("Please add 1 url in as a command line argument")
     exit(1)
 
-# check for a valid url
+url = sys.argv[1]
+if not url.startswith("https://") and not url.startswith("http://"):
+    print("Url MUST start with \"http://\" or \"https://\"")
+    exit(1)
 
-res = requests.get(f"https://{sys.argv[1]}")
+res = requests.get(url)
 res.raise_for_status()
+soup = bs4.BeautifulSoup(res.text,"html.parser")
+a_list = soup.select("a")
 
-# Parse the html
-# Find all links
-# download all thoses links and report any 404s.
+if len(a_list) == 0:
+    print("There are no anchors on the page.")
+    exit(0)
+
+for a in a_list:
+    test_link = a.attrs["href"]
+    try:    
+        if not test_link.startswith("https://") and not test_link.startswith("http://"):
+            link = requests.get(url + test_link) # Relative path
+        else:
+            link = requests.get(test_link)
+    except KeyboardInterrupt:
+        print("Exiting:...")
+        exit(1)
+    except Exception as err:
+        print(f"----> Skipping {test_link}: {err}")
+
+    if link.status_code == 404:
+      print(f"{test_link} was NOT Found")
+    
+
+    # Tested with:
+    # https://google.com
+    # https://example.com
+    # https://badssl.com
